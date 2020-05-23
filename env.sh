4c58720e412a0c74c2d42d7cc9833a95b8af66e3
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2020-05-23 12:03:15 +0530

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PUID=${PUID}
+PGID=${PGID}
+TZ=Asia/Colombo
+URL=meeting.cyber.lk
+SUBDOMAINS=sso,
+VALIDATION=http
+EMAIL=chinthaka@cybergate.lk
+DHLEVEL=2048
+ONLY_SUBDOMAINS=true
+STAGING=false
+POSTGRES_PASSWORD=$(openssl rand -hex 8)
+KEYCLOAK_PASSWORD=$(openssl rand -hex 8)
