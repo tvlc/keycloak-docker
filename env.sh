a2c59ec04cb5a22d11a23227d802be7fbaabcab6
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2020-05-23 12:15:29 +0530

    Update env.sh

--- env.sh
+++ env.sh
@@ -11,3 +11,17 @@ ONLY_SUBDOMAINS=true
 STAGING=false
 POSTGRES_PASSWORD=$(openssl rand -hex 8)
 KEYCLOAK_PASSWORD=$(openssl rand -hex 8)
+
+cat << EOF > ./.env
+PUID=${PUID}
+PGID=${PGID}
+TZ=Asia/Colombo
+URL=meeting.cyber.lk
+SUBDOMAINS=sso,
+VALIDATION=http
+EMAIL=chinthaka@cybergate.lk
+DHLEVEL=2048
+ONLY_SUBDOMAINS=true
+STAGING=false
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+KEYCLOAK_PASSWORD=${KEYCLOAK_PASSWORD}
